a3dce310a83c26f318d20eb4e72f5a8982e7d196
JennyShen056 <112578566+JennyShen056@users.noreply.github.com>
2024-12-29 18:26:43 +0800

    Update data.py

--- src/alignment/data.py
+++ src/alignment/data.py
@@ -46,7 +46,9 @@ def apply_chat_template(
     auto_insert_empty_system_msg: bool = True,
 ):
     if task in ["sft", "generation"]:
-        messages = example["messages"]
+        # messages = example["messages"]
+        messages = example["chosen"]
+
         # We add an empty system message if there is none
         if auto_insert_empty_system_msg:
             maybe_insert_system_message(messages, tokenizer)
